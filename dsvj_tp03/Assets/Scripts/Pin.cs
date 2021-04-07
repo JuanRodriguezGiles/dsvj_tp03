b4556d7e5e44ff6b6f6cef7d83e8c3da36ee9c88
Juan <juanrodriguezgiles@gmail.com>
2021-04-07 20:11:52 -0300

    Added trigger collider to check if Pins fall

--- dsvj_tp03/Assets/Scripts/Pin.cs
+++ dsvj_tp03/Assets/Scripts/Pin.cs
@@ -12,10 +12,6 @@ public class Pin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.transform.rotation.eulerAngles.x);
-        if (transform.rotation.eulerAngles.x >= 80 )
-            Debug.Log("cai2");
-        if (transform.rotation.eulerAngles.z >= 80)
-            Debug.Log("cai2");
+
     }
 }
