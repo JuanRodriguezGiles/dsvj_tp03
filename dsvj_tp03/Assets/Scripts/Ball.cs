aaa27bcdda146ec603c0566d25359d94a095a36c
Juan <juanrodriguezgiles@gmail.com>
2021-04-07 18:05:13 -0300

    Updated ball script to add instant force

--- dsvj_tp03/Assets/Scripts/Ball.cs
+++ dsvj_tp03/Assets/Scripts/Ball.cs
@@ -3,18 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Ball : MonoBehaviour
 {
+    [SerializeField] private float force = 0f;
+    private bool rolling = false;
     void Start()
     {
-
+        GetComponent<ConstantForce>();
     }
     void Update()
     {
-        float hor = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(hor, 0, 0);
-        transform.position += direction * Time.deltaTime;
-        if (transform.position.x <= -3f)
-            transform.position = new Vector3(-3, transform.position.y, transform.position.z);
-        if (transform.position.x >= 3f)
-            transform.position = new Vector3(3, transform.position.y, transform.position.z);
+        if (!rolling)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rolling = false;
+                GetComponent<ConstantForce>().force = new Vector3(0, 0, force);
+                return;
+            }
+
+            float hor = Input.GetAxis("Horizontal");
+            Vector3 direction = new Vector3(hor, 0, 0);
+            transform.position += direction * Time.deltaTime;
+            if (transform.position.x <= -3f)
+                transform.position = new Vector3(-3, transform.position.y, transform.position.z);
+            if (transform.position.x >= 3f)
+                transform.position = new Vector3(3, transform.position.y, transform.position.z);
+
+            force += Input.GetAxis("Vertical") / 50;
+            if (force < 0)
+                force = 0;
+        }
     }
 }
