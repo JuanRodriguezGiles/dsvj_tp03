08a8d71580848d0eb964f63ed7f729e5df5d16de
Juan <juanrodriguezgiles@gmail.com>
2021-04-07 19:37:18 -0300

    Switched plane for cube in order to use collision detection

---
+++ dsvj_tp03/Assets/Scripts/Pin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.transform.rotation.eulerAngles.x);
+        if (transform.rotation.eulerAngles.x >= 80 )
+            Debug.Log("cai2");
+        if (transform.rotation.eulerAngles.z >= 80)
+            Debug.Log("cai2");
+    }
+}
