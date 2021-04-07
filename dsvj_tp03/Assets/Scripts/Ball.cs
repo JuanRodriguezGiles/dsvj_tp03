7b73eb5efa983b7adf7e85fc614dc49b4fa445aa
Juan <juanrodriguezgiles@gmail.com>
2021-04-07 17:41:12 -0300

    Added logic for ball to not go out of bounds

--- dsvj_tp03/Assets/Scripts/Ball.cs
+++ dsvj_tp03/Assets/Scripts/Ball.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Ball : MonoBehaviour
 {
-    ConstantForce force;
     void Start()
     {
-        force = GetComponentInChildren<ConstantForce>();
-        force.force = new Vector3(0, 0, 1);
+
     }
     void Update()
     {
-       
+        float hor = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(hor, 0, 0);
+        transform.position += direction * Time.deltaTime;
+        if (transform.position.x <= -3f)
+            transform.position = new Vector3(-3, transform.position.y, transform.position.z);
+        if (transform.position.x >= 3f)
+            transform.position = new Vector3(3, transform.position.y, transform.position.z);
     }
 }
