289f8fe76ee5cb51791a7701fa3d0e7541d3ba7c
Juan <juanrodriguezgiles@gmail.com>
2021-04-07 16:56:30 -0300

    Added floor and ball

---
+++ dsvj_tp03/Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    ConstantForce force;
+    void Start()
+    {
+        force = GetComponentInChildren<ConstantForce>();
+        force.force = new Vector3(0, 0, 1);
+    }
+    void Update()
+    {
+       
+    }
+}
