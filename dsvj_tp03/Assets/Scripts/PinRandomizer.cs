ac116a1f0af923af15d4614207eaef0cefccce45
Juan <juanrodriguezgiles@gmail.com>
2021-04-11 17:55:57 -0300

    Added basic pin randomizer for positions

---
+++ dsvj_tp03/Assets/Scripts/PinRandomizer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PinRandomizer : MonoBehaviour
+{
+    public GameObject ball;
+    private Ball ballScript;
+    public List<Transform> pinsTransforms;
+    void Start()
+    {
+        ballScript = ball.GetComponent<Ball>();
+    }
+    void Update()
+    {
+        if (!ballScript.rolling && Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (var t in pinsTransforms)
+            {
+                Vector3 newPosVector3;
+                newPosVector3.x = Random.Range(-2f, 2f);
+                newPosVector3.y = 1.2f;
+                newPosVector3.z = Random.Range(0,30);
+                t.position = newPosVector3;
+            }
+        }
+    }
+}
