25e5bb4dabb37d8311002c464934769a0d9aa7ea
Juan <juanrodriguezgiles@gmail.com>
2021-04-11 17:38:18 -0300

    Updated pins positions and board size

--- dsvj_tp03/Assets/Scripts/Ball.cs
+++ dsvj_tp03/Assets/Scripts/Ball.cs
@@ -30,10 +30,10 @@ public class Ball : MonoBehaviour
             float hor = Input.GetAxis("Horizontal");
             Vector3 direction = new Vector3(hor, 0, 0);
             transform.position += direction * Time.deltaTime;
-            if (transform.position.x <= -3f)
-                transform.position = new Vector3(-3, transform.position.y, transform.position.z);
-            if (transform.position.x >= 3f)
-                transform.position = new Vector3(3, transform.position.y, transform.position.z);
+            if (transform.position.x <= -2f)
+                transform.position = new Vector3(-2, transform.position.y, transform.position.z);
+            if (transform.position.x >= 2f)
+                transform.position = new Vector3(2, transform.position.y, transform.position.z);
 
             force += Input.GetAxis("Vertical") / 50;
             if (force < 0)
