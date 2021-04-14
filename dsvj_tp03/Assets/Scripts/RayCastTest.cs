7569053d4639ff1dc98b7aa2a78ab8b6d1de8be9
Juan <juanrodriguezgiles@gmail.com>
2021-04-14 14:42:20 -0300

    Removed unused bool in RayCastTest scene

--- dsvj_tp03/Assets/Scripts/RayCastTest.cs
+++ dsvj_tp03/Assets/Scripts/RayCastTest.cs
@@ -7,7 +7,6 @@ public class RayCastTest : MonoBehaviour
 {
     private Camera mainCamera;
     public GameObject cubeGameObject;
-    private bool destroyed = false;
     // Start is called before the first frame update
     void Start()
     {
