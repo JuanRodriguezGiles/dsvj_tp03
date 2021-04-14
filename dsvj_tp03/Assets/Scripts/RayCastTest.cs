d8f60e9bcd66e55f830d8f8080f45a9837398561
Juan <juanrodriguezgiles@gmail.com>
2021-04-14 14:23:15 -0300

    Added RayCastTest scene

---
+++ dsvj_tp03/Assets/Scripts/RayCastTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RayCastTest : MonoBehaviour
+{
+    private Camera mainCamera;
+    public GameObject cubeGameObject;
+    private bool destroyed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        Vector3 mousePos = Input.mousePosition;
+        Ray ray = mainCamera.ScreenPointToRay(mousePos);
+        Debug.DrawRay(ray.origin, ray.direction * 50, Color.yellow);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 200))
+            {
+                if (hit.collider.gameObject.tag == "Ball")
+                    GameObject.Destroy(hit.collider.gameObject);
+                else
+                    Instantiate(cubeGameObject, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}
